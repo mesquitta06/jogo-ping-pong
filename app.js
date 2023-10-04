a2e8c2a9c39fecfd32589d36bd3ca1931d580dba
Otavio Ferreira de Mesquita <mesquitta.otavio@gmail.com>
2023-10-04 18:19:46 -0300

    Criando a bolinha app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-function setup(){
-    createCanvas(800, 400);
+let x_Bola = 300;
+let y_Bola = 200;
+let diametro = 20;
+
+
+function setup() {
+  createCanvas(600, 400);
 }
 
-function draw(){
-    background(220);
+function draw() {
+  background(0);
+  circle(x_Bola,y_Bola,diametro);
 }
