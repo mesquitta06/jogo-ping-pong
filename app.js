890160ed7080d8e1c4dc09f6d228d610120cc80c
Otavio Ferreira De Mesquita <otaviom.prestserv@vibraenergia.com.br>
2023-10-05 17:46:49 -0300

    Movimentação finalizada

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let x_Bola = 300;
-let y_Bola = 200;
+let y_Bola = 40;
 let diametro = 20;
+
+let velocidadeXBola = 6;
+let velocidadeYBola = 4;
+
+let raio =  diametro / 2;
 
 
 function setup() {
@@ -10,4 +15,19 @@ function setup() {
 function draw() {
   background(0);
   circle(x_Bola,y_Bola,diametro);
+  
+  x_Bola += velocidadeXBola ;
+  
+  y_Bola += velocidadeYBola ;
+  
+  
+  
+  if(x_Bola + raio > width || x_Bola - raio < 0){
+    velocidadeXBola *= -1;
+  }
+  
+  if(y_Bola  + raio > height || y_Bola - raio  < 0){
+    velocidadeYBola *= -1;
+  }
+  
 }
